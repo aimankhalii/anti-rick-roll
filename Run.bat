fc247d3d65661ffa6fc2a346b88b89a952af7e9e
aimankhalii <98976419+aimankhalii@users.noreply.github.com>
2022-08-01 15:20:44 +0100

    Create Run.bat

---
+++ Run.bat
@@ -0,0 +1,63 @@
+@echo off
+title first tool
+color A
+Echo you got rick rolled
+
+set url="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwienNuS6KX5AhVmQ0EAHcUTAjIQyCl6BAgVEAM&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU"
+start microsoft-edge:%url%
+
+echo We're no strangers to love
+echo You know the rules and so do I (do I)
+echo A full commitment's what I'm thinking of
+echo You wouldn't get this from any other guy
+echo I just wanna tell you how I'm feeling
+echo Gotta make you understand
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo We've known each other for so long
+echo Your heart's been aching, but you're too shy to say it (say it)
+echo Inside, we both know what's been going on (going on)
+echo We know the game and we're gonna play it
+echo And if you ask me how I'm feeling
+echo Don't tell me you're too blind to see
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo We've known each other for so long
+echo Your heart's been aching, but you're too shy to say it (to say it)
+echo Inside, we both know what's been going on (going on)
+echo We know the game and we're gonna play it
+echo I just wanna tell you how I'm feeling
+echo Gotta make you understand
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+echo Never gonna give you up
+echo Never gonna let you down
+echo Never gonna run around and desert you
+echo Never gonna make you cry
+echo Never gonna say goodbye
+echo Never gonna tell a lie and hurt you
+pause
